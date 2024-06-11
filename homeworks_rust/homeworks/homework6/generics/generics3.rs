196d15466056819db774076cb843f30128d97916
Patrick <patrick@emotiv.com>
2024-06-12 01:22:25 +0800

    Commit Homework 6

--- homeworks_rust/homeworks/homework6/generics/generics3.rs
+++ homeworks_rust/homeworks/homework6/generics/generics3.rs
@@ -10,18 +10,17 @@
 
 // Execute 'rustlings hint generics3' for hints!
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: Option<T>,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display + std::fmt::Debug> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+            &self.student_name, &self.student_age, &self.grade.as_ref().unwrap())
     }
 }
 
@@ -32,7 +31,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Some(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -46,7 +45,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Some("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
